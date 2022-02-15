0f402d023e0225368d2d25d5a42f5fdbceac5d6a
Nabin Khanal <er.nabinkhanal@gmail.com>
2022-02-15 17:15:46 +0545

    implemented editing input and stack of expressions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,23 +4,32 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) ,on_result(false),
-      editing_input(false) {
+      on_edit(false), s_count(-1), cp(0) {
 	ui->setupUi(this);
 }
 
 MainWindow::~MainWindow() { delete ui; }
 
 void MainWindow::on_op_button_clicked() {
-	if (on_result && !editing_input) {
+    if (on_result && !on_edit) {
         c.display_input.clear();
         c.calculate_input.clear();
-        ui->Input->clear();
+        c.display_input+="Ans";
+        c.calculate_input+="  ~";
+        ui->Input->setText(QString::fromStdString(c.display_input));
 		on_result = false;
 	}
 }
-
+void MainWindow::on_digit_button_clicked(){
+    if (on_result && !on_edit) {
+        c.display_input.clear();
+        c.calculate_input.clear();
+        ui->Input->clear();
+        on_result = false;
+    }
+}
 void MainWindow::on_zero_clicked() {
-	on_op_button_clicked();
+    on_digit_button_clicked();
 	int cp = ui->Input->cursorPosition();
     c.calculate_input.insert(cp, "0");
     c.display_input.insert(cp, "0");
@@ -29,7 +38,7 @@ void MainWindow::on_zero_clicked() {
 }
 
 void MainWindow::on_one_clicked() {
-    on_op_button_clicked();
+    on_digit_button_clicked();
     int cp = ui->Input->cursorPosition();
     c.calculate_input.insert(cp, "1");
     c.display_input.insert(cp, "1");
@@ -38,7 +47,7 @@ void MainWindow::on_one_clicked() {
 }
 
 void MainWindow::on_two_clicked() {
-    on_op_button_clicked();
+    on_digit_button_clicked();
     int cp = ui->Input->cursorPosition();
     c.calculate_input.insert(cp, "2");
     c.display_input.insert(cp, "2");
@@ -47,7 +56,7 @@ void MainWindow::on_two_clicked() {
 }
 
 void MainWindow::on_three_clicked() {
-    on_op_button_clicked();
+    on_digit_button_clicked();
     int cp = ui->Input->cursorPosition();
     c.calculate_input.insert(cp, "3");
     c.display_input.insert(cp, "3");
@@ -56,7 +65,7 @@ void MainWindow::on_three_clicked() {
 }
 
 void MainWindow::on_four_clicked() {
-    on_op_button_clicked();
+    on_digit_button_clicked();
     int cp = ui->Input->cursorPosition();
     c.calculate_input.insert(cp, "4");
     c.display_input.insert(cp, "4");
@@ -65,7 +74,7 @@ void MainWindow::on_four_clicked() {
 }
 
 void MainWindow::on_five_clicked() {
-    on_op_button_clicked();
+    on_digit_button_clicked();
     int cp = ui->Input->cursorPosition();
     c.calculate_input.insert(cp, "5");
     c.display_input.insert(cp, "5");
@@ -74,7 +83,7 @@ void MainWindow::on_five_clicked() {
 }
 
 void MainWindow::on_six_clicked() {
-    on_op_button_clicked();
+    on_digit_button_clicked();
     int cp = ui->Input->cursorPosition();
     c.calculate_input.insert(cp, "6");
     c.display_input.insert(cp, "6");
@@ -83,7 +92,7 @@ void MainWindow::on_six_clicked() {
 }
 
 void MainWindow::on_seven_clicked() {
-    on_op_button_clicked();
+    on_digit_button_clicked();
     int cp = ui->Input->cursorPosition();
     c.calculate_input.insert(cp, "7");
     c.display_input.insert(cp, "7");
@@ -101,7 +110,7 @@ void MainWindow::on_eight_clicked() {
 }
 
 void MainWindow::on_nine_clicked() {
-    on_op_button_clicked();
+    on_digit_button_clicked();
     int cp = ui->Input->cursorPosition();
     c.calculate_input.insert(cp, "9");
     c.display_input.insert(cp, "9");
@@ -155,12 +164,13 @@ void MainWindow::on_minus_clicked() {
 }
 
 void MainWindow::on_del_clicked() {
+    if(c.display_input.size() > 0){
     int cp = ui->Input->cursorPosition();
 
     c.display_input.erase(cp-1, 1);
     c.calculate_input.erase(cp-1, 1);
     ui->Input->setText(QString::fromStdString(c.display_input));
-
+}
 }
 
 void MainWindow::on_clear_clicked() {
@@ -168,19 +178,31 @@ void MainWindow::on_clear_clicked() {
     c.calculate_input.clear();
     ui->Input->clear();
     on_result = false;
+    on_edit = false;
 }
 
 void MainWindow::on_equal_clicked() {
     try {
+        if(c.calculate_input == ""){
+            return;
+        }
         double value = c.calculate(nullptr);
         ui->Result->display(value);
-    }  catch (...) {
-        ui->Result->display("error");
+        calculate_stack.push_back(c.calculate_input);
+        display_stack.push_back(c.display_input);
+        s_count = display_stack.size() - 1;
+        c.previous_answer = value;
+    }  catch (std::domain_error d) {
+        std::cout << d.what() << std::endl;
+        ui->Result->display(d.what());
         c.display_input.clear();
         c.calculate_input.clear();
         ui->Input->clear();
+    } catch(...){
+        ui->Result-> display("error");
     }
 	on_result = true;
+    on_edit = false;
 }
 
 void MainWindow::on_inverse_clicked()
@@ -241,5 +263,58 @@ void MainWindow::on_right_b_clicked()
     c.display_input.insert(cp, ")");
 
     ui->Input->setText(QString::fromStdString(c.display_input));
+}
+
+
+void MainWindow::on_up_clicked()
+{
+     if (on_result&&!calculate_stack.empty() && s_count>0){
+         s_count--;
+        c.calculate_input = calculate_stack[s_count];
+        c.display_input = display_stack[s_count];
+        ui->Input->setText(QString::fromStdString(c.display_input));
+    }
+}
+
+
+void MainWindow::on_down_clicked()
+{
+    if(on_result && s_count < calculate_stack.size()-1){
+        s_count++;
+        c.calculate_input = calculate_stack[s_count];
+        c.display_input = display_stack[s_count];
+        ui->Input->setText(QString::fromStdString(c.display_input));
+    }
+}
+
+
+void MainWindow::on_left_clicked()
+{
+    std::cout << ui->Input->cursorPosition() << std::endl;
+    ui->Input->setFocus();
+    ui->Input->cursorBackward(false);
+    on_edit = true;
+}
+
+
+void MainWindow::on_right_clicked()
+{
+    std::cout << ui->Input->cursorPosition() << std::endl;
+    ui->Input->setFocus();
+    ui->Input->cursorForward(false);
+    on_edit = true;
+
+}
+
+void MainWindow::on_ans_clicked()
+{
+    on_op_button_clicked();
+    int cp = ui->Input->cursorPosition();
+    c.calculate_input.insert(cp, "  ~");
+    c.display_input.insert(cp, "Ans");
+    ui->Input->setText(QString::fromStdString(c.display_input));
+    ui->Input->setCursorPosition(cp);
+    std::cout << cp << std::endl;
+    on_edit = false;
 }
 
